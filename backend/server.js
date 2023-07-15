e7c4b646b5fe18ec0da1f8bb4155537a408c2a8a
kalsang-Rad <kalsangwrk@gmail.com>
2023-07-15 14:41:54 +0545

    Env files

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,34 @@
-const express = require("express");
-const app = express();
-// const dietRoutes = require("./routes/dietRoutes")
-const { default: mongoose } = require("mongoose");
+require("dotenv").config();
 
+// packages
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const dietRoutes = require("./routes/dietRoutes");
+
+// express app
+const app = express();
+
+// middleware
 app.use(express.json());
 
-// app.use("/api/diets", dietRoutes);
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/diets", dietRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("connected to database");
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log("listening on requests on port ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
